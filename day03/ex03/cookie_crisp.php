63b9cfd080df7810e695c8a434fd80f8c991602f
Damian ODENDAAL <dodendaa@c5r9s4.wethinkcode.co.za>
2019-10-13 16:36:49 +0200

    done

---
+++ day03/ex03/cookie_crisp.php
@@ -0,0 +1,20 @@
+<?PHP
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cookie_crisp.php                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dodendaa <dodendaa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/13 10:42:32 by dodendaa          #+#    #+#             */
+/*   Updated: 2019/10/13 10:42:33 by dodendaa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+if ($_GET['action'] == "set")
+    setcookie($_GET['name'], $_GET['value'], time() + 10);
+if ($_GET['action'] == "get")
+    echo $_COOKIE[$_GET['name']];
+if ($_GET['action'] == "del")
+    setcookie($_GET['name'], $_GET['value'], time() - 10);
+?>
