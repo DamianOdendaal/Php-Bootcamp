f65a75960cebd5b04dfbd03400ee4e2f27f819be
Damian ODENDAAL <dodendaa@c5r9s4.wethinkcode.co.za>
2019-10-16 15:39:02 +0200

    splif life

--- rush00/index.php
+++ rush00/index.php
@@ -44,19 +44,19 @@
         </br></br></br>
 		<table>
 				<tr>
-					<td><img src = "https://cdn.shopify.com/s/files/1/1090/7524/products/flower-space-nugs-high-potency-cannabis-2.jpg?v=1544594796" height = "20%" width = "40%"><br/><span>Space Nugs</span></td>
-					<td><img src =  "http://catnews.org/weed/wp-content/uploads/2017/02/202-5.jpg" height = "20%" width = "40%"><br/><span>Cannabis Chocolate Bar</span></td>
-					<td><img src = "https://live.staticflickr.com/5558/30200040824_b1538c1ebd_b.jpg" height = "20%" width = "40%"><br/><span>Grape Fruit</span></td>
+					<td><img src = "https://cdn.shopify.com/s/files/1/1090/7524/products/flower-space-nugs-high-potency-cannabis-2.jpg?v=1544594796" height = "20%" width = "40%"><br/><span>Space Nugs</br>R85.00</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src =  "http://catnews.org/weed/wp-content/uploads/2017/02/202-5.jpg" height = "20%" width = "40%"><br/><span>Cannabis Chocolate Bar</br>R35.00</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src = "https://live.staticflickr.com/5558/30200040824_b1538c1ebd_b.jpg" height = "20%" width = "40%"><br/><span>Grape Fruit</br>R50.00</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
 				</tr>
 				<tr>
-					<td><img src = "https://www.mailorder-marijuana.ca/wp-content/uploads/2018/10/IMG_0410.jpg" height = "20%" width = "40%"><br/><span>White Widow</span></td>
-					<td><img src = "https://cdn.shopify.com/s/files/1/2636/1928/products/upload-100240-00628242050203-00_1200x.jpg?v=1567085995" height = "20%" width = "40%"><br/><span>Northern Lights</span></td>
-					<td><img src = "https://i2.wp.com/www.420curefam.com/wp-content/uploads/2018/05/420curefam-Sour-Diesel-Marijuana.jpg?fit=1292%2C969&ssl=1" height = "20%" width = "40%"><br/><span>Sour Diesel</span></td>
+					<td><img src = "https://www.mailorder-marijuana.ca/wp-content/uploads/2018/10/IMG_0410.jpg" height = "20%" width = "40%"><br/><span>White Widow</br>R100.00</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src = "https://cdn.shopify.com/s/files/1/2636/1928/products/upload-100240-00628242050203-00_1200x.jpg?v=1567085995" height = "20%" width = "40%"><br/><span>Northern Lights</br>R49.99</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src = "https://i2.wp.com/www.420curefam.com/wp-content/uploads/2018/05/420curefam-Sour-Diesel-Marijuana.jpg?fit=1292%2C969&ssl=1" height = "20%" width = "40%"><br/><span>Sour Diesel</br>R49.99</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
                 </tr>
                 <tr>
-					<td><img src = "https://potcargo.com/uploads/2019/02/gold-flowers-triangle-kush-group-190208-1200x1200.jpg" height = "20%" width = "40%"><br/><span>WTF</span></td>
-					<td><img src = "https://cdn.greenrush.com/media/8/4/8/1576848/triangle-kush-24-thc-5297.jpg" height = "30%" width = "40%"><br/><span>Barck O Bubba</span></td>
-					<td><img src = "https://shop.herbalremediesil.com/wp-content/uploads/2019/01/flower-bermudatriangle-img-2865-1.jpg" height = "20%" width = "40%"><br/><span>Laughing Grass</span></td>
+					<td><img src = "https://potcargo.com/uploads/2019/02/gold-flowers-triangle-kush-group-190208-1200x1200.jpg" height = "20%" width = "40%"><br/><span>WTF</br>R79.99</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src = "https://cdn.greenrush.com/media/8/4/8/1576848/triangle-kush-24-thc-5297.jpg" height = "30%" width = "40%"><br/><span>Barck O Bubba</br>R49.99</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
+					<td><img src = "https://shop.herbalremediesil.com/wp-content/uploads/2019/01/flower-bermudatriangle-img-2865-1.jpg" height = "20%" width = "40%"><br/><span>Laughing Grass</br>R59.99</br><button>add to cart<i class="fa fa-shopping-cart" style="font-size:24px;"></i></button></span></td>
 				</tr>
             </table>
     
